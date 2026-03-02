2b3f2a7d80a4fb7c82626b602e6bc281aefcde29
Prof.SamsonW Quantum <profsamsonw_quantum@kali.Redmi9C.net>
2026-03-02 16:26:39 +0300

    Save local changes

---
+++ api/payments.js
@@ -0,0 +1,9 @@
+// api/payments.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Payments API working' });
+});
+
+module.exports = router;
