03f1b539fdfa13566b40b85025a5b5655562e5ad
Prof.SamsonW Quantum <profsamsonw_quantum@kali.Redmi9C.net>
2026-03-03 00:50:38 +0300

    Add correctPassword method to User model

--- models/User.js
+++ models/User.js
@@ -117,8 +117,8 @@ userSchema.pre('save', async function(next) {
     next();
 });
 
-userSchema.methods.comparePassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
+userSchema.methods.correctPassword = async function(candidatePassword,userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
 };
 
 module.exports = mongoose.model('User', userSchema);
